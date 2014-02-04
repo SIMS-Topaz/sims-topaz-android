12becf6ae2c551518745705e006ad8cae9047950
veve <alina.gherman04@gmail.com>
2014-02-04 17:52:07 +0100

    support map

--- android/src/com/sims/topaz/MainActivity.java
+++ android/src/com/sims/topaz/MainActivity.java
@@ -1,14 +1,18 @@
 package com.sims.topaz;
 import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.SupportMapFragment;
+
 import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
 import android.app.Activity;
 
-public class MainActivity extends Activity {
-
+public class MainActivity extends FragmentActivity {
+	//I extend FragmentActivity and not Activity in order to have Support MapFragment
 	private GoogleMap mMap;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mMap = ((SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map)).getMap();
     }
 }

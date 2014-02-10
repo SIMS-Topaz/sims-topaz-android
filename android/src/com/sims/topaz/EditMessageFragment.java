161b14dc29d9bc92fbb289b0371019ace75a6f3a
Jean-Baptiste Ghoul <jean-baptiste.ghoul@insa-lyon.fr>
2014-02-10 23:11:49 +0100

    Edit Message : button Send and Cancel

--- android/src/com/sims/topaz/EditMessageFragment.java
+++ android/src/com/sims/topaz/EditMessageFragment.java
@@ -1,6 +1,5 @@
 package com.sims.topaz;
 
-import java.sql.Timestamp;
 import java.util.Date;
 import java.util.List;
 
@@ -12,10 +11,14 @@ import com.sims.topaz.network.modele.Preview;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.text.InputFilter.LengthFilter;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.view.View.OnClickListener;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class EditMessageFragment extends Fragment
 				implements NetworkDelegate{
@@ -31,8 +34,27 @@ public class EditMessageFragment extends Fragment
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
         Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_edit_message, container, false);
+		View view = inflater.inflate(R.layout.fragment_edit_message, container, false);
+		setUpButtons(view);
+        return view;
     }    
+	
+	private void setUpButtons(View view) {
+		Button send = (Button) view.findViewById(R.id.button_send_message);
+		send.setOnClickListener(new OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				onSendButton(v);
+			}
+		});
+		Button cancel = (Button) view.findViewById(R.id.button_cancel_message);
+		cancel.setOnClickListener(new OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				getFragmentManager().popBackStack();
+			}
+		});
+	}
     
     public void onSendButton(View view) {
     	EditText editText = (EditText) getActivity().findViewById(R.id.editMessage);
@@ -53,6 +75,8 @@ public class EditMessageFragment extends Fragment
 	@Override
 	public void afterPostMessage(Message message) {
 		// TODO Auto-generated method stub
+		Toast.makeText(getActivity(), "Message envoyé", Toast.LENGTH_SHORT).show();
+		getFragmentManager().popBackStack();
 		
 	}
 

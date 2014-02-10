3b407e09d3c11a3ac4b0d4bdf18e7d559b510358
bbouchardon <benjamin.bouchardon@insa-lyon.fr>
2014-02-10 16:02:12 +0100

    Clustering

---
+++ android/src/com/sims/topaz/PreviewClusterItem.java
@@ -0,0 +1,33 @@
+package com.sims.topaz;
+
+import com.google.android.gms.maps.model.LatLng;
+import com.google.maps.android.clustering.ClusterItem;
+import com.sims.topaz.network.modele.Preview;
+
+public class PreviewClusterItem implements ClusterItem {
+    private final LatLng mPosition;
+    private Preview preview;
+    private String tag;
+
+    public PreviewClusterItem(Preview preview) {
+    	this.preview = preview;
+        mPosition = new LatLng(preview.getLatitude(), preview.getLongitude());
+    }
+
+    @Override
+    public LatLng getPosition() {
+        return mPosition;
+    }
+    
+    public void setTag(String tag) {
+		this.tag = tag;
+	}
+    
+    public String getTag() {
+		return tag;
+	}
+    
+    public Preview getPreview() {
+		return preview;
+	}
+}

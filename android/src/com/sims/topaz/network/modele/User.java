a623330111446118735a507b0aca1142db8126e5
bbouchardon <benjamin.bouchardon@insa-lyon.fr>
2014-02-17 15:12:36 +0100

    User model

---
+++ android/src/com/sims/topaz/network/modele/User.java
@@ -0,0 +1,48 @@
+package com.sims.topaz.network.modele;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class User {
+	
+	private Long id;
+	private String name;
+	private String password;
+	private String email;
+	
+	public User() {
+	}
+	
+	public Long getId() {
+		return id;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	
+	public String getPassword() {
+		return password;
+	}
+	
+	public String getEmail() {
+		return email;
+	}
+	
+	public void setId(Long id) {
+		this.id = id;
+	}
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	
+}

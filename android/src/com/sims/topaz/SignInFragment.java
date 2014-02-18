4c3a43e29274738f14a937458fcbbd6350443d1b
veve <alina.gherman04@gmail.com>
2014-02-18 21:13:33 +0100

    error fields in sign in

--- android/src/com/sims/topaz/SignInFragment.java
+++ android/src/com/sims/topaz/SignInFragment.java
@@ -11,6 +11,8 @@ import com.sims.topaz.utils.MyPreferencesUtilsSingleton;
 import com.sims.topaz.utils.SimsContext;
 
 import android.support.v4.app.Fragment;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.KeyEvent;
@@ -23,12 +25,13 @@ import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
-public class SignInFragment extends Fragment implements SignInDelegate, ErreurDelegate{
+public class SignInFragment extends Fragment implements SignInDelegate, ErreurDelegate, TextWatcher{
 	private EditText mUserNameEditText;
 	private EditText mPasswordEditText;
 	private NetworkRestModule mRestModule;
 	private Button mLoginButton;
-
+	private TextView mUserNameErrorTextView;
+	private TextView mPasswordErrorTextView;
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -37,7 +40,12 @@ public class SignInFragment extends Fragment implements SignInDelegate, ErreurDe
 		mUserNameEditText = (EditText)v.findViewById(R.id.sign_in_username);
 		mPasswordEditText = (EditText)v.findViewById(R.id.sign_in_password);
 		mLoginButton = (Button)v.findViewById(R.id.Sign_in);
-
+		mUserNameErrorTextView = (TextView)v.findViewById(R.id.sign_in_username_error);
+		mPasswordErrorTextView = (TextView)v.findViewById(R.id.sign_in_password_error);
+		//set TextWatcher listeners
+		mUserNameEditText.addTextChangedListener(this);
+		mPasswordEditText.addTextChangedListener(this);
+		
 		//set the done button to fields
 		TextView.OnEditorActionListener listener=new TextView.OnEditorActionListener() {
 			@Override
@@ -54,7 +62,6 @@ public class SignInFragment extends Fragment implements SignInDelegate, ErreurDe
 			public void onClick(View v) {signInAction();}
 		});
 		mUserNameEditText.setOnEditorActionListener(listener);
-		mPasswordEditText.setOnEditorActionListener(listener);
 
 		//set the username if the user already made an account 
 		if(MyPreferencesUtilsSingleton.getInstance(SimsContext.getContext())
@@ -84,9 +91,9 @@ public class SignInFragment extends Fragment implements SignInDelegate, ErreurDe
 					password);	
 			mRestModule.signinUser(u);
 		}else if(!AuthUtils.isValidUsername(user)) {
-			//TODO show error field
+			mUserNameErrorTextView.setVisibility(View.VISIBLE);
 		}else if(!AuthUtils.isValidPassword(password, 6)) {
-			//TODO show error field
+			mPasswordErrorTextView.setVisibility(View.VISIBLE);
 		}
 	}
 	@Override
@@ -112,5 +119,26 @@ public class SignInFragment extends Fragment implements SignInDelegate, ErreurDe
 		checkInput(mUserNameEditText.getText().toString(),
 				mPasswordEditText.getText().toString());
 	}
+
+	@Override
+	public void afterTextChanged(Editable s) {
+		String user = mUserNameEditText.getText().toString();
+		if(!user.equals("") && !AuthUtils.isValidUsername(user)) {
+			mUserNameErrorTextView.setVisibility(View.VISIBLE);
+		}else {
+			mUserNameErrorTextView.setVisibility(View.GONE);
+		}
+		
+		if(AuthUtils.isValidPassword(mPasswordErrorTextView.getText().toString(), 6)) {
+			mPasswordErrorTextView.setVisibility(View.GONE);
+		}
+	}
+
+	@Override
+	public void beforeTextChanged(CharSequence s, int start, int count,
+			int after) {}
+
+	@Override
+	public void onTextChanged(CharSequence s, int start, int before, int count) {}
 
 }

49bef371f31fa2e1d889fdf973d00000aac61803
veve <alina.gherman04@gmail.com>
2014-02-18 11:58:23 +0100

    shared preferences (not tested due to network error)

--- android/src/com/sims/topaz/AuthActivity.java
+++ android/src/com/sims/topaz/AuthActivity.java
@@ -1,5 +1,11 @@
 package com.sims.topaz;
 
+import com.sims.topaz.network.NetworkRestModule;
+import com.sims.topaz.network.interfaces.ErreurDelegate;
+import com.sims.topaz.network.interfaces.SignInDelegate;
+import com.sims.topaz.network.modele.ApiError;
+import com.sims.topaz.network.modele.User;
+import com.sims.topaz.utils.MyPreferencesUtilsSingleton;
 import com.sims.topaz.utils.SimsContext;
 
 import android.content.Intent;
@@ -10,19 +16,42 @@ import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.app.FragmentTransaction;
 
-public class AuthActivity extends FragmentActivity{
+public class AuthActivity extends FragmentActivity implements SignInDelegate, ErreurDelegate{
 	private Button mTempButton;
 	private Fragment signInFragment;
 	private Fragment signUpFragment;
+	private NetworkRestModule mRestModule;
 	/**
 	 * Whether or not we're showing the back of the card (otherwise showing the
 	 * front).
 	 */
 	private boolean mShowingBack = false;
+	
+    @Override
+    public void onStart(){
+        super.onStart();
+        SimsContext.setContext(getApplicationContext());       
+        if(MyPreferencesUtilsSingleton.getInstance(SimsContext.getContext())
+        		.hasKey(MyPreferencesUtilsSingleton.SHARED_PREFERENCES_AUTH_USERNAME) && 
+        		MyPreferencesUtilsSingleton.getInstance(SimsContext.getContext())
+        		.hasKey(MyPreferencesUtilsSingleton.SHARED_PREFERENCES_AUTH_USERNAME)){
+        	mRestModule = new NetworkRestModule(this);
+        	String username = MyPreferencesUtilsSingleton.getInstance(SimsContext.getContext())
+            		.getString(MyPreferencesUtilsSingleton.SHARED_PREFERENCES_AUTH_USERNAME, "");
+        	String password = MyPreferencesUtilsSingleton.getInstance(SimsContext.getContext())
+            		.getString(MyPreferencesUtilsSingleton.SHARED_PREFERENCES_AUTH_PASSWORD, "");
+        	User user = new User();
+        	user.setPassword(password);
+        	user.setName(username);
+        	mRestModule.signinUser(user);
+        }
+        
+    
+    }
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		SimsContext.setContext(getApplicationContext());
+		
 		setContentView(R.layout.activity_auth);
 		mTempButton = (Button)findViewById(R.id.go_to_map);
 		mTempButton.setOnClickListener(new View.OnClickListener() {			
@@ -61,5 +90,21 @@ public class AuthActivity extends FragmentActivity{
 					.addToBackStack(null)
 					.commit();	
 		}
+	}
+	@Override
+	public void apiError(ApiError error) {
+		// TODO Auto-generated method stub
+		
+	}
+	@Override
+	public void networkError() {
+		// TODO Auto-generated method stub
+		
+	}
+	@Override
+	public void afterSignIn(User user) {
+		Intent intent = new Intent(SimsContext.getContext(),
+				DrawerActivity.class);
+		startActivity(intent);	
 	}
 }

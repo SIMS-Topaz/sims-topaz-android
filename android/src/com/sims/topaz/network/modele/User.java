3dcf2c7195fa70fd84ad5ffb11d5b737bb8e3bd6
veve <alina.gherman04@gmail.com>
2014-03-03 12:37:32 +0100

    fields for auth page

--- android/src/com/sims/topaz/network/modele/User.java
+++ android/src/com/sims/topaz/network/modele/User.java
@@ -1,74 +1,79 @@
 package com.sims.topaz.network.modele;
 
+import java.util.List;
+
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class User {
-	
+
 	@JsonProperty("user_id")
 	private Long id;
-	
+
 	@JsonProperty("user_name")
 	private String user_name;
-	
+
 	@JsonProperty("user_password")
 	private String password;
-	
+
 	@JsonProperty("user_email")
 	private String email;
-	
+
 	@JsonProperty("user_verified")
 	private Boolean verified;
-	
+
 	@JsonProperty("user_picture")
 	private String pictureUrl;
 
 	@JsonProperty("user_comments")
-	private Comment comments;	
-
+	private List<Comment> comments;	
+	
+	@JsonProperty("user_likes")
+	private List<Comment> likes;	
+	
 	@JsonProperty("user_status")
 	private String status;		
-	
+
 	public User() {
 	}
 
 	public Long getId() {
 		return id;
 	}
-	
+
 	public String getUserName() {
 		return user_name;
 	}
-	
+
 	public String getPassword() {
 		return password;
 	}
-	
+
 	public String getEmail() {
 		return email;
 	}
-	
+
 	public Boolean getVerified() {
 		return verified;
 	}
-	
+
 	public void setId(Long id) {
 		this.id = id;
 	}
-	
+
 	public void setUserName(String name) {
 		this.user_name = name;
 	}
-	
+
 	public void setPassword(String password) {
 		this.password = password;
 	}
-	
+
 	public void setEmail(String email) {
 		this.email = email;
 	}
-	
+
 	public void setVerified(Boolean verified) {
 		this.verified = verified;
 	}
@@ -77,9 +82,6 @@ public class User {
 		return pictureUrl;
 	}
 
-	public Comment getComments() {
-		return comments;
-	}
 
 	public String getStatus() {
 		return status;
@@ -88,7 +90,7 @@ public class User {
 	@Override
 	public String toString(){
 		return String.valueOf(id)+", "
-	+this.user_name + ", " + this.password + ", "+ this.email;
+				+this.user_name + ", " + this.password + ", "+ this.email;
 	}
-	
+
 }

20a493774831049561351e64c7e697ae56532476
Jean-Baptiste Ghoul <jean-baptiste.ghoul@insa-lyon.fr>
2014-03-09 17:24:36 +0100

    preview adapter view holder

--- android/src/com/sims/topaz/adapter/PreviewListAdapter.java
+++ android/src/com/sims/topaz/adapter/PreviewListAdapter.java
@@ -1,15 +1,20 @@
 package com.sims.topaz.adapter;
 
+import java.sql.Date;
 import java.util.List;
 
 import android.content.Context;
+import android.text.format.DateFormat;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 
 import com.sims.topaz.R;
 import com.sims.topaz.network.modele.Preview;
+import com.sims.topaz.utils.MyTypefaceSingleton;
 
 public class PreviewListAdapter extends ArrayAdapter<Preview> {
 
@@ -35,27 +40,38 @@ public class PreviewListAdapter extends ArrayAdapter<Preview> {
 			view = inflater.inflate(R.layout.adapter_preview_item, null);
 		}
 		if(position >= 0){
-//			holder.mUserName = (TextView) view.findViewById(R.id.comment_item_person_name);
-//			holder.mUserName.setTypeface(MyTypefaceSingleton.getInstance().getTypeFace());
-//			
-//			holder.mUserComment = (TextView) view.findViewById(R.id.comment_item_text);
-//			holder.mUserComment.setTypeface(MyTypefaceSingleton.getInstance().getTypeFace());
-//			
-//			holder.mCommentDate = (TextView) view.findViewById(R.id.comment_item_time);
-//			holder.mCommentDate.setTypeface(MyTypefaceSingleton.getInstance().getTypeFace());
+			holder.mUserName = (TextView) view.findViewById(R.id.preview_item_username);
+			holder.mUserName.setTypeface(MyTypefaceSingleton.getInstance().getTypeFace());
+			
+			holder.mText = (TextView) view.findViewById(R.id.preview_item_text);
+			holder.mText.setTypeface(MyTypefaceSingleton.getInstance().getTypeFace());
+			
+			holder.mDate = (TextView) view.findViewById(R.id.preview_item_time);
+			holder.mDate.setTypeface(MyTypefaceSingleton.getInstance().getTypeFace());
+			
+			holder.mLikeBar = (ProgressBar) view.findViewById(R.id.preview_item_like_bar);
+			
+			holder.mNote = (TextView) view.findViewById(R.id.preview_item_note);
+			holder.mNote.setTypeface(MyTypefaceSingleton.getInstance().getTypeFace());
+			
 			if(getItem(position)!=null){
 				Preview p = getItem(position);
-//				holder.mUserName.setText(ci.getUser());
-//				holder.mUserComment.setText(ci.getCommentText());
-//				holder.mCommentDate.setText(DateFormat.format
-//						(getContext().getString(R.string.date_format), 
-//								new Date( ci.getDate()) ) );
+				holder.mUserName.setText(p.getUserName());
+				holder.mText.setText(p.getText());
+				holder.mDate.setText(DateFormat.format
+						(getContext().getString(R.string.date_format), 
+								new Date( p.getTimestamp()) ) );
 			}
 		}
 		return view;
 	}
 	
-	class ViewHolder {  
+	class ViewHolder {
+		TextView mUserName;
+		TextView mDate;
+		TextView mText;
+		ProgressBar mLikeBar;
+		TextView mNote;
 		
     }  
 

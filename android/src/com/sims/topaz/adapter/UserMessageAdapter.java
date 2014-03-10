d3f4c742a953ea62e023a3cddf89e95371fd9bfd
veve <alina.gherman04@gmail.com>
2014-03-10 23:55:20 +0100

    fix user info bugs

--- android/src/com/sims/topaz/adapter/UserMessageAdapter.java
+++ android/src/com/sims/topaz/adapter/UserMessageAdapter.java
@@ -18,13 +18,20 @@ import com.sims.topaz.utils.CameraUtils;
 import com.sims.topaz.utils.MyTypefaceSingleton;
 
 public class UserMessageAdapter extends ArrayAdapter<Message>  {
-	
+
+	private int count = 0;
 	private byte[] image;
 	public UserMessageAdapter(Context mDelegate, int resource, List<Message> messagesList,byte[] mImage) {
 		super(mDelegate, resource, messagesList);
 		this.image = mImage;
+		count = messagesList.size();
 	}	
-
+	
+	@Override
+	public int getCount() {
+		return count;
+	}
+	
 	public View getView(int position, View convertView, ViewGroup parent){
 		View view = convertView;
 		ViewHolder holder = null; 
@@ -37,13 +44,13 @@ public class UserMessageAdapter extends ArrayAdapter<Message>  {
 		if(position >= 0){
 			holder.mUserName = (TextView) view.findViewById(R.id.message_item_person_name);
 			holder.mUserName.setTypeface(MyTypefaceSingleton.getInstance().getTypeFace());
-			
+
 			holder.mUserMessage = (TextView) view.findViewById(R.id.message_item_text);
 			holder.mUserMessage.setTypeface(MyTypefaceSingleton.getInstance().getTypeFace());
-			
+
 			holder.mMessageDate = (TextView) view.findViewById(R.id.message_item_time);
 			holder.mMessageDate.setTypeface(MyTypefaceSingleton.getInstance().getTypeFace());
-			
+
 			holder.mUserImage = (ImageView) view.findViewById(R.id.message_item_image);
 			if(image!=null){
 				holder.mUserImage.setImageDrawable(CameraUtils.getDrwableFromBytes(image));
@@ -59,11 +66,11 @@ public class UserMessageAdapter extends ArrayAdapter<Message>  {
 		}
 		return view;
 	}
-	
+
 	class ViewHolder {  
-		 TextView mUserName;
-		 TextView mUserMessage;
-		 TextView mMessageDate;
-		 ImageView mUserImage;
-    }  
+		TextView mUserName;
+		TextView mUserMessage;
+		TextView mMessageDate;
+		ImageView mUserImage;
+	}  
 }

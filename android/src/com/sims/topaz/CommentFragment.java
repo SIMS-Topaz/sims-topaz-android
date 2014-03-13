830a7f62d13027e7d67d84308aa0f396b9e5907b
veve <alina.gherman04@gmail.com>
2014-03-13 19:26:46 +0100

    retain fragments

--- android/src/com/sims/topaz/CommentFragment.java
+++ android/src/com/sims/topaz/CommentFragment.java
@@ -97,7 +97,12 @@ implements MessageDelegate,CommentDelegate,OnShowUserProfile,LoadPictureTaskInte
 		return fragment;
 	}
 	
-	
+	@Override
+	public void onCreate(Bundle savedInstanceState){
+		super.onCreate(savedInstanceState);
+		setRetainInstance(true);
+
+	}
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,
 			Bundle savedInstanceState) {
